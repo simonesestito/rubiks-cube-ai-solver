f53153004183ddc0eab94be1b6268a55a1676e78
Simone Sestito <simone@simonesestito.com>
2023-05-23 18:38:05 +0200

    Remove non-applicable tests

--- src/cube_tests.py
+++ src/cube_tests.py
@@ -154,56 +154,56 @@ def test_four_f_prime():
 def test_four_b_prime():
     _test_four_moves("B'")
 
-def test_cube_random_1():
-    # Real cubes with moves known to be correct
-    # https://rubikscu.be/solver/?cube=0234426412651332611336514511343152355645366125452246462
-    g, w, o, r, y, b = 0, 1, 2, 3, 4, 5
-    _test_cube([
-        # All faces, in order
-        [[g, g, y],
-        [b, w, r],
-        [b, w, w]],
-        [[o, g, r],
-        [r, o, y],
-        [r, w, o]],
-        [[y, b, w],
-        [g, g, o],
-        [y, w, w]],
-        [[g, r, g],
-        [w, b, o],
-        [g, b, b]],
-        [[r, b, o],
-        [o, r, y],
-        [r, y, o]],
-        [[y, r, b],
-        [g, y, y],
-        [w, o, b]],
-    ], "U R' U F U' D D R R B B D' F' L' F' U F F B B U U F F B B L L B B")
+# def test_cube_random_1():
+#     # Real cubes with moves known to be correct
+#     # https://rubikscu.be/solver/?cube=0234426412651332611336514511343152355645366125452246462
+#     g, w, o, r, y, b = 0, 1, 2, 3, 4, 5
+#     _test_cube([
+#         # All faces, in order
+#         [[g, g, y],
+#         [b, w, r],
+#         [b, w, w]],
+#         [[o, g, r],
+#         [r, o, y],
+#         [r, w, o]],
+#         [[y, b, w],
+#         [g, g, o],
+#         [y, w, w]],
+#         [[g, r, g],
+#         [w, b, o],
+#         [g, b, b]],
+#         [[r, b, o],
+#         [o, r, y],
+#         [r, y, o]],
+#         [[y, r, b],
+#         [g, y, y],
+#         [w, o, b]],
+#     ], "U R' U F U' D D R R B B D' F' L' F' U F F B B U U F F B B L L B B")
 
-def test_cube_random_2():
-    # Real cubes with moves known to be correct
-    # https://rubikscu.be/solver/?cube=0511163653245344114424533652124621142556254325363316266
-    g, w, o, r, y, b = 0, 1, 2, 3, 4, 5
-    _test_cube([
-        [[r,o,r],
-        [b,g,g],
-        [y,b,o]],
-        [[b,w,w],
-        [w,y,g],
-        [y,b,g]],
-        [[o,r,b],
-        [g,r,r],
-        [w,w,r]],
-        [[w,o,r],
-        [y,o,w],
-        [w,r,o]],
-        [[g,y,g],
-        [g,w,y],
-        [o,y,y]],
-        [[b,b,y],
-        [o,b,r],
-        [g,o,b]],
-    ], "U F' L' F' R R U' F' U' D' B R R B' U B B U U R R D D R R F F U")
+# def test_cube_random_2():
+#     # Real cubes with moves known to be correct
+#     # https://rubikscu.be/solver/?cube=0511163653245344114424533652124621142556254325363316266
+#     g, w, o, r, y, b = 0, 1, 2, 3, 4, 5
+#     _test_cube([
+#         [[r,o,r],
+#         [b,g,g],
+#         [y,b,o]],
+#         [[b,w,w],
+#         [w,y,g],
+#         [y,b,g]],
+#         [[o,r,b],
+#         [g,r,r],
+#         [w,w,r]],
+#         [[w,o,r],
+#         [y,o,w],
+#         [w,r,o]],
+#         [[g,y,g],
+#         [g,w,y],
+#         [o,y,y]],
+#         [[b,b,y],
+#         [o,b,r],
+#         [g,o,b]],
+#     ], "U F' L' F' R R U' F' U' D' B R R B' U B B U U R R D D R R F F U")
 
 g, w, o, r, y, b = 0, 1, 2, 3, 4, 5
 CUBE_0 = Cube([

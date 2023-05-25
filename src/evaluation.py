8d516042864429771347ff29d05ddf98bfdda9ca
Simone Sestito <simone@simonesestito.com>
2023-05-25 08:25:35 +0200

    Cross validation (fold = 5)

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,44 +1,77 @@
-from cubes_map import CubesMap
-from cube import Cube
-from cubes_dataset import CUBE_MOVES_ENCODING
-import random
-from progressbar import progressbar
-from training import model, PYTORCH_DEVICE
+import training
+from model import CubeModel
+import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, SubsetRandomSampler
+import torch.optim
+from sklearn.model_selection import KFold
+import numpy as np
 
-if 'known_cubes' not in globals():
-  known_cubes = CubesMap(filename = './cubes_map.bin')
-else:
-  print('Cubes map already loaded')
+num_folds = 5
 
-moves = [*CUBE_MOVES_ENCODING.keys()][:-1]
+indices = np.arange(len(training.dataset))
+kfold = KFold(n_splits=num_folds, shuffle=True)
 
-def randomize_cube(n_moves = 8):
-    cube = Cube()
-    for _ in range(n_moves):
-        move = random.choice(moves)
-        cube.perform_action(move)
-    return cube
-
-if __name__ == '__main__':
-    model.eval() #! Disable training mode
-    total_cubes = 0
-    correct_cubes = 0
-
-    for _ in progressbar(range(100_000)):
-        cube = randomize_cube()
-
-        next_move = random.choice(moves)
-        cube.perform_action(next_move)
-
-        if known_cubes[cube] is None:
-            total_cubes += 1
-            prediction = model(cube.to_tensor().to(PYTORCH_DEVICE))
-            y = nn.Softmax(dim=1)(prediction).argmax(1).item()
-            if y == CUBE_MOVES_ENCODING[next_move]:
-                correct_cubes += 1
-            else:
-                print(y, end=' ')
-    
+for fold, (train_indices, test_indices) in enumerate(kfold.split(indices)):
     print()
-    print(f'Accuracy: {correct_cubes / total_cubes * 100:.4f}% (= {correct_cubes} / {total_cubes})')
+    print(f'Fold {fold}:')
+    print('\tTrain samples:', len(train_indices))
+    print('\tTest samples:', len(test_indices))
+
+    model = CubeModel().to(training.PYTORCH_DEVICE)
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=training.LEARNING_RATE)
+
+    # Create samplers for the training and test sets
+    train_sampler = SubsetRandomSampler(train_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+
+    train_loader = DataLoader(
+            training.dataset,
+            batch_size=training.BATCH_SIZE,
+            sampler=train_sampler,
+    )
+    test_loader = DataLoader(
+            training.dataset,
+            batch_size=training.BATCH_SIZE,
+            sampler=test_sampler,
+    )
+
+    # Train the model using training data
+    for epoch in range(training.EPOCHS):
+        print('Training epoch:', epoch)
+        model = model.train()
+        for batch, (X, y) in enumerate(train_loader):
+            if batch % 200 == 0:
+                print('\tTraining with batch:', batch)
+            X = X.to(training.PYTORCH_DEVICE)
+            y = y.to(training.PYTORCH_DEVICE)
+            
+            pred = model(X)
+            loss = criterion(pred, y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    # Test with test set
+    model = model.eval()
+    with torch.no_grad():
+        print('Testing')
+        total_samples, correct_samples = 0, 0
+        for batch, (X, y) in enumerate(test_loader):
+            if batch % 200 == 0:
+                print('\tTesting with batch:', batch)
+            X = X.to(training.PYTORCH_DEVICE)
+            y = y.to(training.PYTORCH_DEVICE)
+
+            pred = model(X)
+
+            _, predicted = torch.max(pred.data, 1)
+            total_samples += y.size(0)
+            correct_samples += (predicted == y).sum().item()
+        acc = correct_samples * 100 / total_samples
+        print(f'Test accuracy: {acc:.4f} ({correct_samples} / {total_samples}')
+
+
